d8c3cd25e708caeacc0a9dae491d1ecb54c335dc
Luan Ferreira <luanhsouzaf@gmail.com>
2025-09-21 17:09:53 -0300

    🎉 feat: commit inicial do projeto

---
+++ src/main/java/com/luanferreira/App.java
@@ -0,0 +1,13 @@
+package com.luanferreira;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
